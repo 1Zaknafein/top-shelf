01880e685b7eb10e9a2bed3fef865c7f561996f6
1Zaknafein <98032843+1Zaknafein@users.noreply.github.com>
2026-03-28 19:10:52 +0000

    Added DELETE option for gameApiRequest
    Allows clients to call db with DELETE method

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import { Game } from "@/types/types";
 
 export async function gameApiRequest(
-  method: "POST" | "PUT",
+  method: "POST" | "PUT" | "DELETE",
   body: Partial<Game>,
 ): Promise<Game> {
   const res = await fetch("/api/games", {
@@ -9,6 +9,5 @@ export async function gameApiRequest(
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
   });
-
   return res.json();
 }
